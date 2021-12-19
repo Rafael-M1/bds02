e7867fd0b2c6cc8d28b5ffaaac6fb240f8583453
Rafael-M1 <rafael.muzzi2@gmail.com>
2021-12-19 01:47:38 -0400

    Tests bds02

--- src/main/java/com/devsuperior/bds02/dto/CityDTO.java
+++ src/main/java/com/devsuperior/bds02/dto/CityDTO.java
@@ -1,14 +1,20 @@
 package com.devsuperior.bds02.dto;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.entities.Event;
 
 public class CityDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
 	private String name;
+	
+	private List<EventDTO> events = new ArrayList<>();
 	
 	public CityDTO() {
 	}
@@ -21,6 +27,11 @@ public class CityDTO implements Serializable {
 	public CityDTO(City entity) {
 		id = entity.getId();
 		name = entity.getName();
+	}
+	
+	public CityDTO(City entity, Set<Event> events) {
+		this(entity);
+		events.forEach(cat -> this.events.add(new EventDTO(cat)) );
 	}
 
 	public Long getId() {
